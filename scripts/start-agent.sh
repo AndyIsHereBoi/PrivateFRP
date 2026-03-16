5657b176125b778c3fbac26ed31ef2f86b87a4e1
AndyIsHereBoi <andyaaron2ndgrade@gmail.com>
2026-03-16 01:36:04 -0500

    new git changes on startup

--- scripts/start-agent.sh
+++ scripts/start-agent.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -e
 cd "$(dirname "$0")/.."
+git reset --hard HEAD
+git pull
 docker compose -f docker-compose.agent.yml --env-file agent.env up --build
--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -e
 cd "$(dirname "$0")/.."
+git reset --hard HEAD
+git pull
 docker compose -f docker-compose.yml --env-file server.env up --build
